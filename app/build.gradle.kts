d02e7189ef26474a6300a6d29b951a402e030fd9
liupeng <>
2024-03-04 16:33:14 +0800

    uniapp push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(files("libs/MiPush_SDK_Client_6_0_1-C_3rd.aar"))
 }
--- app/src/main/java/easemob/com/uniapp/UniPush.java
+++ app/src/main/java/easemob/com/uniapp/UniPush.java
@@ -1,7 +1,14 @@
 package easemob.com.uniapp;
 
+import org.jetbrains.annotations.NotNull;
+
+import kotlin.Unit;
+
 public class UniPush {
-  public void xiaomi(){
+
+
+  public static void xiaomi() {
+
 
   }
 }
