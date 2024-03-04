f0d38f411125d67d4130a86ba90514047abdff82
liupeng <>
2024-03-04 15:25:21 +0800

    uniapp push

---
+++ app/src/main/java/easemob/com/uniapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package easemob.com.uniapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
