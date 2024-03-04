9b2a1d15365fb13a54c5a138294e2d92013b76c7
liupeng <>
2024-03-04 15:46:39 +0800

    uniapp push

--- uniapp-push/build.gradle.kts
+++ uniapp-push/build.gradle.kts
@@ -35,4 +35,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(files("libs/MiPush_SDK_Client_6_0_1-C_3rd.aar"))
 }
